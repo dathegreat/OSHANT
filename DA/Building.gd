38ff0e3ff262b2c7b7acf4152a10f336c8c8f285
D.A <d.a.thegreat123@gmail.com>
2023-10-09 17:22:01 -0600

    Adds explosive scene to represent a demo charge

--- DA/Building.gd
+++ DA/Building.gd
@@ -13,6 +13,9 @@ var brick_size: Vector3 = Vector3(brick_width, brick_height, brick_depth)
 func _ready():
 	for step in range(0, steps):
 		for y in range(0, height):
+			#determine if the given brick should be created or left empty
+			if randi_range(0, 100) < 5:
+				continue
 			#the center of the cylinder
 			var center: Vector3 = Vector3(0, y * brick_height, 0)
 			var theta: float = rotation_step_size * step
@@ -21,24 +24,24 @@ func _ready():
 			brick.mesh.size = brick_size
 			brick.mesh.material = StandardMaterial3D.new()
 			brick.mesh.material.albedo_color = Color(lerpf(0, 1, float(step) / steps), y / height, 0)
+			var collision_shape = CollisionShape3D.new()
+			collision_shape.shape = BoxShape3D.new()
+			collision_shape.shape.size = brick_size
+			var rigid_body = RigidBody3D.new()
 			#move each brick to its given position on the cylinder and then
 			#point its face toward the center of the cylinder
-			brick.look_at_from_position(
+			rigid_body.look_at_from_position(
 				Vector3(
 					cylinder_radius * cos(theta), 
 					y * brick_height,  
 					cylinder_radius * sin(theta)
 				), 
 				center)
-			var collision_shape = CollisionShape3D.new()
-			collision_shape.shape = BoxShape3D.new()
-			collision_shape.shape.size = brick_size
-			var rigid_body = RigidBody3D.new()
 			rigid_body.freeze = true
 			rigid_body.add_child(collision_shape)
 			rigid_body.add_child(brick)
 			add_child(rigid_body)
-			
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
