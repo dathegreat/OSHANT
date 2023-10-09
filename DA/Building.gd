cfb6154e3aaaa78ddf38b369fa5e8749ac9ac6be
D.A <d.a.thegreat123@gmail.com>
2023-10-09 14:12:07 -0600

    Basic building now spawns nicely

--- DA/Building.gd
+++ DA/Building.gd
@@ -1,35 +1,37 @@
 extends Node3D
 
-var center: Vector3 = Vector3(0, 0, 0)
-var steps: int = 20
+var steps: int = 200
 var rotation_step_size: float = (2 * PI) / steps
 var cylinder_radius: float = 2
-var height: float = 10
-var brick_height = 1
-var brick_width = (2 * PI * cylinder_radius) / steps
-var brick_depth = 0.05
-var brick_size: Vector3 = Vector3(brick_height, brick_width, brick_depth)
+var height: float = 20
+var brick_height: float = 0.5
+var brick_width: float = (2 * PI * cylinder_radius) / steps
+var brick_depth: float = 0.05
+var brick_size: Vector3 = Vector3(brick_width, brick_height, brick_depth)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for step in range(0, steps):
-			for y in range(0, height):
-				var brick: MeshInstance3D = MeshInstance3D.new()
-				brick.mesh = BoxMesh.new()
-				brick.mesh.size = brick_size
-				var theta: float = rotation_step_size * step
-				brick.set_position( 
-					Vector3(
-						cylinder_radius * cos(theta), 
-						y * brick_height,  
-						cylinder_radius * sin(theta)
-					)
-				)
-				var rotation: Vector3 = brick.position.direction_to(center)
-				#TODO for tomorrow: fix rotation logic
-				brick.rotate_object_local(Vector3(0, 1, 0), rotation.y)
-				add_child(brick)
-	print(rotation_step_size, " ", steps)
+		for y in range(1, height):
+			#the center of the cylinder
+			var center: Vector3 = Vector3(0, y * brick_height, 0)
+			var theta: float = rotation_step_size * step
+			var brick: MeshInstance3D = MeshInstance3D.new()
+			brick.mesh = BoxMesh.new()
+			brick.mesh.size = brick_size
+			brick.mesh.material = StandardMaterial3D.new()
+			brick.mesh.material.albedo_color = Color(lerpf(0, 1, float(step) / steps), y / height, 0)
+			#move each brick to its given position on the cylinder and then
+			#point its face toward the center of the cylinder
+			brick.look_at_from_position(
+				Vector3(
+					cylinder_radius * cos(theta), 
+					y * brick_height,  
+					cylinder_radius * sin(theta)
+				), 
+				center)
+			#add brick to scene
+			add_child(brick)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- DA/TestEnvironment.gd
+++ DA/TestEnvironment.gd
@@ -4,16 +4,28 @@ var building_node = preload("res://DA/Building.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var building = building_node.instantiate()
-	add_child(building)
+	get_node("RigidBody3D").add_child(building)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("move_right"):
-		get_node("Camera3D").position.x += 0.1
-	if Input.is_action_pressed("move_left"):
-		get_node("Camera3D").position.x += -0.1
-	if Input.is_action_pressed("move_up"):
-		get_node("Camera3D").position.z += 0.1
-	if Input.is_action_pressed("move_down"):
-		get_node("Camera3D").position.z += -0.1
+	if Input.is_key_pressed(KEY_D):
+		get_node("Camera3D").position += Vector3.RIGHT * 0.1
+	if Input.is_key_pressed(KEY_A):
+		get_node("Camera3D").position += Vector3.RIGHT * -0.1
+	if Input.is_key_pressed(KEY_W):
+		get_node("Camera3D").position += Vector3.FORWARD * 0.1
+	if Input.is_key_pressed(KEY_S):
+		get_node("Camera3D").position += Vector3.FORWARD * -0.1
+	if Input.is_key_pressed(KEY_Q):
+		get_node("Camera3D").position.y += 0.1
+	if Input.is_key_pressed(KEY_E):
+		get_node("Camera3D").position.y += -0.1
+	if Input.is_key_pressed(KEY_UP):
+		get_node("Camera3D").rotate_object_local(Vector3(1, 0, 0), 0.01)
+	if Input.is_key_pressed(KEY_DOWN):
+		get_node("Camera3D").rotate_object_local(Vector3(1, 0, 0), -0.01)
+	if Input.is_key_pressed(KEY_LEFT):
+		get_node("Camera3D").rotate_object_local(Vector3(0, 1, 0), 0.01)
+	if Input.is_key_pressed(KEY_RIGHT):
+		get_node("Camera3D").rotate_object_local(Vector3(0, 1, 0), -0.01)
