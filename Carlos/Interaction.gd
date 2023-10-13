dca5fa41efd96acf41c374523d748f3c52871139
Karl_Jansky <alatorre.carloseduardomx@gmail.com>
2023-10-13 13:51:18 -0600

    interaction

--- Carlos/Interaction.gd
+++ Carlos/Interaction.gd
@@ -1,5 +1,13 @@
 extends Area3D
 class_name InteractableGame
+@export_enum("next_level", "dialogue", "trigger") var interaction_type : int
 
 func interact():
 	print("I ", self.name, " am interacting!")
+	match interaction_type:
+		0:
+			print("going to next level")
+		1:
+			print("dialogue")
+		2:
+			print("trigger")
