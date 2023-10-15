82cf2ab171cb6c9230aa80da2ce4ca3ebc72eff9
D.A <d.a.thegreat123@gmail.com>
2023-10-15 15:25:07 -0600

    Adds explosive charge placement and end level logic

--- DA/ExplosiveBall.gd
+++ DA/ExplosiveBall.gd
@@ -5,10 +5,16 @@ var sphere_radius: float = 3.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Area3D/CollisionShape3D").shape.radius = sphere_radius
-	get_node("Area3D/CSGSphere3D").radius = sphere_radius
 
 func explode():
+	var flash_sphere = get_node("FlashSphere")
+	var charge_sprite = get_node("ChargeSprite")
+	var explosion_sprite = get_node("ExplosionSprite")
 	await get_tree().create_timer(0.05).timeout
+	flash_sphere.visible = true
+	charge_sprite.visible = false
+	explosion_sprite.visible = true
+	explosion_sprite.play()
 	for body in get_node("Area3D").get_overlapping_bodies():
 		if body is RigidBody3D:
 			body.freeze = false
--- scenes/main_level.gd
+++ scenes/main_level.gd
@@ -7,7 +7,18 @@ func _ready():
 	var player_height = get_node("Building").player_starting_height
 	get_node("RotationAnchor/PlayerCharacter").look_at_from_position(player_position, Vector3(0, player_height, 0))
 	print(player_position)
+	EventBus.end_level.connect(_on_level_end)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_released("right_click"):
+		var explosive = load("res://DA/ExplosiveBall.tscn").instantiate()
+		explosive.set_position(get_node("RotationAnchor/PlayerCharacter").global_position)
+		get_node("ChargeContainer").add_child(explosive)
+	if Input.is_key_pressed(KEY_9):
+		EventBus.emit_signal("end_level")
+
+func _on_level_end():
+	for explosive in get_node("ChargeContainer").get_children():
+		explosive.explode()
+	
